8a93ac0ff363813dc6ae528f64f183e4353f3cc4
Ariel Alexis Lorda Barrabasqui <alexis.lorda@gmail.com>
2022-06-07 23:02:20 -0300

    Styles in button-add

--- simple-crud/src/components/simple-crud-button-add/simple-crud-button-add.tsx
+++ simple-crud/src/components/simple-crud-button-add/simple-crud-button-add.tsx
@@ -1,15 +1,20 @@
+import './simple-crud-button-add'
 import Button from '@mui/material/Button';
+
 
 export default function SimpleCrudButtonAd() {
     return (
         <div>
             <Button
                 variant="contained"
+                fullWidth
+                size='large'
                 color="success"
+                style={{ marginBlockStart: 10 }}
                 onClick={() => { console.log('ADD') }}
             >
                 Add
-            </Button>
-        </div>
+            </Button >
+        </div >
     )
 };
