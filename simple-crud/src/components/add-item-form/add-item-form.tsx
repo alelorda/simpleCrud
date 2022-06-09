f72b9b50ced379179b4c1e22d37dc2ea361e788b
Ariel Alexis Lorda Barrabasqui <alexis.lorda@gmail.com>
2022-06-09 16:35:38 -0300

    added form item

--- simple-crud/src/components/add-item-form/add-item-form.tsx
+++ simple-crud/src/components/add-item-form/add-item-form.tsx
@@ -1,15 +1,34 @@
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-
+import TextField from '@mui/material/TextField';
 
 export const AddItemForm = () => {
     return (
         <div>
             <DialogContent>
-                <DialogContentText id="alert-dialog-slide-description">
-                    Let Google help apps determine location. This means sending anonymous
-                    location data to Google, even when no apps are running.
-                </DialogContentText>
+                <TextField
+                    required
+                    fullWidth
+                    id="outlined-required"
+                    label="Surname"
+                />
+                <TextField
+                    required
+                    fullWidth
+                    id="outlined-required"
+                    label="Name"
+                />
+                <TextField
+                    required
+                    fullWidth
+                    id="outlined-required"
+                    label="Address"
+                />
+                <TextField
+                    required
+                    fullWidth
+                    id="outlined-required"
+                    label="Salary"
+                />
             </DialogContent>
         </div >
     );
